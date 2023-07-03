000bd7b0bdaf337a42f370de62244b427266b232
yang1206 <yang15188280673@outlook.com>
2023-07-03 21:26:29 +0800

    chore: github Actions

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,20 +1,13 @@
 // https://vitepress.dev/guide/custom-theme
 import Theme from 'vitepress/theme'
+import Layout from '../components/Layout.vue'
 import './style.css'
 import 'uno.css'
-import type { Router } from 'vitepress'
-import type { App } from 'vue'
-import Layout from '../components/Layout.vue'
 
 export default {
   ...Theme,
-  Layout() {
-    return h(Layout, null, {
-      // 'home-features-after': () => h(HomeSponsors),
-      // 'aside-ads-before': () => h(AsideSponsors),
-    })
-  },
-  enhanceApp({ app, router }: { app: App; router: Router }) {
-
-  },
+  Layout,
+  // enhanceApp({ app, router, siteData }) {
+  //   // ...
+  // },
 }
