5fd0bb5e9a92dbac8db0c458ee4a992891359523
yang1206 <yang15188280673@outlook.com>
2023-11-19 18:12:06 +0800

    chore: 优化 demo 体验

--- example/src/layouts/default.vue
+++ example/src/layouts/default.vue
@@ -4,7 +4,11 @@ import { useAppStore } from '@/store'
 const { darkMode } = storeToRefs(useAppStore())
 
 const curPage = getCurrentPages().at(-1) as any
-
+const height = ref(
+  // #ifdef H5
+  'calc(100vh - 44px)',
+  // #endif
+)
 const title = computed(() => {
   return curPage.route.split('/')[3]
 })
@@ -36,13 +40,14 @@ function onClickLeft() {
     <!-- #ifdef H5 -->
     <nut-navbar
       :placeholder="true" :fixed="true" :left-show="!!title" safe-area-inset-top
-      :title="title ? title : 'NutUi'" :size="16" custom-style="font-weight: bold;"
-      @on-click-back="onClickLeft"
+      :title="title ? title : 'NutUi'" :size="16" custom-style="font-weight: bold;" @on-click-back="onClickLeft"
     />
     <!-- #endif -->
-    <scroll-view style="height:100vh" class=" n-bg pb-safe pt-safe" scroll-y>
-      <slot />
-    </scroll-view>
+    <nut-backtop :height="height" custom-class=" n-bg pb-safe pt-safe">
+      <template #content>
+        <slot />
+      </template>
+    </nut-backtop>
   </nut-config-provider>
 </template>
 
