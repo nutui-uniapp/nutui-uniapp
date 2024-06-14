7c5c093b4c885f3e5512f6d161d1ab0611ef8f7d
小何同学 <HeDianGeng0601@outlook.com>
2024-06-14 15:39:33 +0800

    refactor: once more

--- packages/nutui/components/griditem/griditem.vue
+++ packages/nutui/components/griditem/griditem.vue
@@ -1,37 +1,52 @@
 <script lang="ts" setup>
-import { type CSSProperties, computed, defineComponent } from 'vue'
+import { type CSSProperties, computed } from 'vue'
 import { getMainClass, getMainStyle, pxCheck } from '../_utils'
-import { CLICK_EVENT, PREFIX } from '../_constants'
+import { CLICK_EVENT } from '../_constants'
 import { GRID_KEY, type GridProps } from '../grid'
 import { useInject, useRouter } from '../_hooks'
 import { gridItemEmits, gridItemProps } from './griditem'
 
+const COMPONENT_NAME = 'nut-grid-item'
+
+// eslint-disable-next-line vue/define-macros-order
+defineOptions({
+  name: COMPONENT_NAME,
+  options: {
+    virtualHost: true,
+    addGlobalClass: true,
+    styleIsolation: 'shared',
+  },
+})
+
 const props = defineProps(gridItemProps)
+
 const emit = defineEmits(gridItemEmits)
-const componentName = `${PREFIX}-grid-item`
 
 const Parent = useInject<{ props: Required<GridProps> }>(GRID_KEY)
+
 const index = Parent.index
 const parent = Parent?.parent?.props
+
 // root
 const classes = computed(() => {
-  return getMainClass(props, componentName)
+  return getMainClass(props, COMPONENT_NAME)
 })
 
 const styles = computed(() => {
-  if (!parent)
-    return
-  const style: CSSProperties = {
-    flexBasis: `${100 / +parent.columnNum}%`,
-  }
+  const style: CSSProperties = {}
 
-  if (parent?.square) {
-    style.paddingTop = `${100 / +parent.columnNum}%`
-  }
-  else if (parent.gutter) {
-    style.paddingRight = pxCheck(parent.gutter)
-    if (index.value >= Number(parent.columnNum))
-      style.marginTop = pxCheck(parent.gutter)
+  if (parent) {
+    style.flexBasis = `${100 / +parent.columnNum}%`
+
+    if (parent.square) {
+      style.paddingTop = `${100 / +parent.columnNum}%`
+    }
+    else if (parent.gutter) {
+      style.paddingRight = pxCheck(parent.gutter)
+
+      if (index.value >= Number(parent.columnNum))
+        style.marginTop = pxCheck(parent.gutter)
+    }
   }
 
   return getMainStyle(props, style)
@@ -39,23 +54,30 @@ const styles = computed(() => {
 
 // content
 const contentClass = computed(() => {
-  if (!parent)
-    return
-  const prefixCls = `${componentName}__content`
-  return {
-    [`${prefixCls}`]: true,
-    [`${prefixCls}--border`]: parent.border,
-    [`${prefixCls}--surround`]: parent.border && parent.gutter,
-    [`${prefixCls}--center`]: parent.center,
-    [`${prefixCls}--square`]: parent.square,
-    [`${prefixCls}--reverse`]: parent.reverse,
-    [`${prefixCls}--${parent.direction}`]: !!parent.direction,
-    [`${prefixCls}--clickable`]: parent.clickable,
+  const prefixCls = `${COMPONENT_NAME}__content`
+
+  const value: Record<string, boolean> = {
+    [prefixCls]: true,
   }
+
+  if (parent) {
+    Object.assign(value, {
+      [`${prefixCls}--border`]: parent.border,
+      [`${prefixCls}--surround`]: parent.border && parent.gutter,
+      [`${prefixCls}--center`]: parent.center,
+      [`${prefixCls}--square`]: parent.square,
+      [`${prefixCls}--reverse`]: parent.reverse,
+      [`${prefixCls}--${parent.direction}`]: !!parent.direction,
+      [`${prefixCls}--clickable`]: parent.clickable,
+    })
+  }
+
+  return value
 })
 
 // click
 const router = useRouter()
+
 function handleClick(event: unknown) {
   emit(CLICK_EVENT, event as MouseEvent)
 
@@ -75,25 +97,15 @@ function handleClick(event: unknown) {
 }
 </script>
 
-<script lang="ts">
-export default defineComponent({
-  name: 'NutGridItem',
-  options: {
-    virtualHost: true,
-    addGlobalClass: true,
-    styleIsolation: 'shared',
-  },
-})
-</script>
-
 <template>
   <view :class="classes" :style="styles" @click="handleClick">
     <view :class="contentClass">
       <slot />
       <view class="nut-grid-item__text">
-        <template v-if="text">
-          {{ text }}
+        <template v-if="props.text">
+          {{ props.text }}
         </template>
+
         <slot v-else name="text" />
       </view>
     </view>
