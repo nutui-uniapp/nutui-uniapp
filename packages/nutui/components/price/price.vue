3f8177f6a25c24f436d7d9eade219aca164d602c
小何同学 <HeDianGeng0601@outlook.com>
2024-08-02 16:46:12 +0800

    refactor: price、progress

--- packages/nutui/components/price/price.vue
+++ packages/nutui/components/price/price.vue
@@ -1,102 +1,97 @@
 <script lang="ts" setup>
-import { computed, defineComponent } from 'vue'
-import { PREFIX } from '../_constants'
+import { computed } from 'vue'
 import { getMainClass } from '../_utils'
 import { priceProps } from './price'
 
-const props = defineProps(priceProps)
+const COMPONENT_NAME = 'nut-price'
 
-const classes = computed(() => {
-  return getMainClass(props, componentName, {
-    [`${componentName}--strike`]: props.strikeThrough,
-  })
-})
-function replaceSpecialChar(url: string) {
-  url = url.replace(/&quot;/g, '"')
-  url = url.replace(/&amp;/g, '&')
-  url = url.replace(/&lt;/g, '<')
-  url = url.replace(/&gt;/g, '>')
-  url = url.replace(/&nbsp;/g, ' ')
-  url = url.replace(/&yen;/g, '￥')
-  return url
-}
-const showSymbol = computed(() => {
-  const symbol = props.needSymbol ? replaceSpecialChar(props.symbol) : ''
-  return symbol
-})
-function checkPoint(price: string | number) {
-  return String(price).indexOf('.') > 0
-}
-
-function formatThousands(num: any) {
-  if (Number(num) === 0)
-    num = 0
-
-  if (checkPoint(num)) {
-    num = Number(num).toFixed(props.decimalDigits)
-    num = typeof num.split('.') === 'string' ? num.split('.') : num.split('.')[0]
-  }
-  else {
-    num = num.toString()
-  }
-  if (props.thousands)
-    return (num || 0).toString().replace(/(\d)(?=(?:\d{3})+$)/g, '$1,')
-  else
-    return num
-}
-function formatDecimal(decimalNum: any) {
-  if (Number(decimalNum) === 0)
-    decimalNum = 0
-
-  if (checkPoint(decimalNum)) {
-    decimalNum = Number(decimalNum).toFixed(props.decimalDigits)
-    decimalNum
-          = typeof decimalNum.split('.') === 'string' ? 0 : decimalNum.split('.')[1] ? decimalNum.split('.')[1] : 0
-  }
-  else {
-    decimalNum = 0
-  }
-  const result = `0.${decimalNum}`
-  const resultFixed = Number(result).toFixed(props.decimalDigits)
-  return String(resultFixed).substring(2, resultFixed.length)
-}
-</script>
-
-<script lang="ts">
-const componentName = `${PREFIX}-price`
-
-export default defineComponent({
-  name: componentName,
+// eslint-disable-next-line vue/define-macros-order
+defineOptions({
+  name: COMPONENT_NAME,
   options: {
     virtualHost: true,
     addGlobalClass: true,
     styleIsolation: 'shared',
   },
 })
+
+const props = defineProps(priceProps)
+
+const classes = computed(() => {
+  return getMainClass(props, COMPONENT_NAME, {
+    [`${COMPONENT_NAME}--strike`]: props.strikeThrough,
+  })
+})
+
+const innerSymbol = computed(() => {
+  if (!props.needSymbol)
+    return ''
+
+  return props.symbol.replace(/&quot;/g, '"')
+    .replace(/&amp;/g, '&')
+    .replace(/&lt;/g, '<')
+    .replace(/&gt;/g, '>')
+    .replace(/&nbsp;/g, ' ')
+    .replace(/&yen;/g, '￥')
+})
+
+function roundTo(value: number, digits: number) {
+  const factor = 10 ** digits
+
+  return Math.round(value * factor) / factor
+}
+
+const innerValue = computed(() => {
+  const stringValue = roundTo(Number(props.price), props.decimalDigits).toFixed(props.decimalDigits)
+
+  let integer: string
+  let decimal: string
+
+  const existPoint = stringValue.includes('.')
+
+  if (existPoint) {
+    const fragments = stringValue.split('.')
+
+    integer = fragments[0]
+    decimal = fragments[1]
+  }
+  else {
+    integer = stringValue
+    decimal = ''
+  }
+
+  if (props.thousands)
+    integer = integer.replace(/(\d)(?=(?:\d{3})+$)/g, '$1,')
+
+  return {
+    integer,
+    decimal,
+  }
+})
 </script>
 
 <template>
-  <view :class="classes" :style="customStyle">
+  <view :class="classes" :style="props.customStyle">
     <rich-text
-      v-if="needSymbol && position === 'before'"
+      v-if="props.needSymbol && props.position === 'before'"
       class="nut-price--symbol"
-      :class="`nut-price--symbol-${size}`"
-      :nodes="showSymbol"
+      :class="`nut-price--symbol-${props.size}`"
+      :nodes="innerSymbol"
     />
-    <view :class="`nut-price--${size}`">
-      {{ formatThousands(price) }}
-    </view>
-    <view v-if="decimalDigits !== 0" :class="`nut-price--decimal-${size}`">
-      .
-    </view>
-    <view :class="`nut-price--decimal-${size}`">
-      {{ formatDecimal(price) }}
-    </view>
+
+    <text :class="`nut-price--${props.size}`">
+      {{ innerValue.integer }}
+    </text>
+
+    <text v-if="innerValue.decimal.length > 0" :class="`nut-price--decimal-${props.size}`">
+      .{{ innerValue.decimal }}
+    </text>
+
     <rich-text
-      v-if="needSymbol && position === 'after'"
+      v-if="props.needSymbol && props.position === 'after'"
       class="nut-price--symbol"
-      :class="`nut-price--symbol-${size}`"
-      :nodes="showSymbol"
+      :class="`nut-price--symbol-${props.size}`"
+      :nodes="innerSymbol"
     />
   </view>
 </template>
--- packages/nutui/components/progress/progress.vue
+++ packages/nutui/components/progress/progress.vue
@@ -1,95 +1,165 @@
 <script lang="ts" setup>
-import { computed, defineComponent, useSlots } from 'vue'
-import { PREFIX } from '../_constants'
+import type { CSSProperties } from 'vue'
+import { computed, useSlots } from 'vue'
 import NutIcon from '../icon/icon.vue'
-import { getMainClass } from '../_utils'
+import { getMainClass, pxCheck } from '../_utils'
 import { progressProps } from './progress'
 
-const props = defineProps(progressProps)
+const COMPONENT_NAME = 'nut-progress'
 
-const slotDefault = !!useSlots().default
-const classes = computed(() => {
-  return getMainClass(props, componentName)
-})
-const height = computed(() => {
-  if (props.strokeWidth)
-    return `${props.strokeWidth}px`
-
-  return undefined
-})
-const percentage = computed(() => {
-  return Number(props.percentage) >= 100 ? 100 : Number(props.percentage)
-})
-const bgStyle = computed(() => {
-  return {
-    width: `${percentage.value}%`,
-    background: props.strokeColor || '',
-  }
-})
-const textStyle = computed(() => {
-  return {
-    color: props.textColor || '',
-  }
-})
-</script>
-
-<script lang="ts">
-const componentName = `${PREFIX}-progress`
-
-export default defineComponent({
-  name: componentName,
+// eslint-disable-next-line vue/define-macros-order
+defineOptions({
+  name: COMPONENT_NAME,
   options: {
     virtualHost: true,
     addGlobalClass: true,
     styleIsolation: 'shared',
   },
 })
+
+const props = defineProps(progressProps)
+
+const slots = useSlots()
+
+const innerValue = computed(() => {
+  return Math.max(0, Math.min(100, Number(props.percentage)))
+})
+
+const innerText = computed(() => {
+  return `${innerValue.value}${props.isShowPercentage ? '%' : ''}`
+})
+
+const classes = computed(() => {
+  return getMainClass(props, COMPONENT_NAME)
+})
+
+const outerClasses = computed<Record<string, boolean>>(() => {
+  return {
+    'nut-progress-outer-part': props.showText && !props.textInside,
+    [`nut-progress-${props.size}`]: true,
+  }
+})
+
+const innerClasses = computed<Record<string, boolean>>(() => {
+  return {
+    'nut-active': props.status === 'active',
+  }
+})
+
+const outerHeight = computed(() => {
+  if (props.strokeWidth)
+    return pxCheck(props.strokeWidth)
+
+  return undefined
+})
+
+const outerStyles = computed(() => {
+  const value: CSSProperties = {}
+
+  if (outerHeight.value != null)
+    value.height = outerHeight.value
+
+  return value
+})
+
+const innerStyles = computed(() => {
+  const value: CSSProperties = {
+    width: `${innerValue.value}%`,
+  }
+
+  if (props.strokeColor)
+    value.background = props.strokeColor
+
+  return value
+})
+
+const textStyles = computed(() => {
+  const value: CSSProperties = {
+    left: `${innerValue.value}%`,
+    transform: `translate(-${innerValue.value}%,-50%)`,
+    background: props.textBackground || props.strokeColor,
+  }
+
+  if (outerHeight.value != null)
+    value.lineHeight = outerHeight.value
+
+  return value
+})
+
+const pureTextStyles = computed(() => {
+  const value: CSSProperties = {}
+
+  if (props.textColor)
+    value.color = props.textColor
+
+  return value
+})
+
+const outsideTextStyles = computed(() => {
+  const value: CSSProperties = {}
+
+  if (outerHeight.value != null)
+    value.lineHeight = outerHeight.value
+
+  return value
+})
+
+const slotStyles = computed<CSSProperties>(() => {
+  return {
+    position: 'absolute',
+    top: '50%',
+    left: `${innerValue.value}%`,
+    transform: `translate(-${innerValue.value}%,-50%)`,
+  }
+})
 </script>
 
 <template>
-  <div :class="classes" :style="customStyle">
-    <div
-      class="nut-progress-outer"
-      :class="[showText && !textInside ? 'nut-progress-outer-part' : '', size ? `nut-progress-${size}` : '']"
-      :style="{ height }"
+  <view :class="classes" :style="props.customStyle">
+    <view class="nut-progress-outer" :class="outerClasses" :style="outerStyles">
+      <view class="nut-progress-inner" :class="innerClasses" :style="innerStyles" />
+
+      <template v-if="props.showText && props.textInside">
+        <view
+          v-if="!slots.default"
+          class="nut-progress-text nut-progress-insidetext"
+          :style="textStyles"
+        >
+          <text :style="pureTextStyles">
+            {{ innerText }}
+          </text>
+        </view>
+
+        <view v-else class="nut-progress-slot" :style="slotStyles">
+          <slot />
+        </view>
+      </template>
+    </view>
+
+    <view
+      v-if="props.showText && !props.textInside"
+      class="nut-progress-text"
+      :style="outsideTextStyles"
     >
-      <div class="nut-progress-inner" :class="[status === 'active' ? 'nut-active' : '']" :style="bgStyle" />
-      <div
-        v-if="showText && textInside && !slotDefault"
-        class="nut-progress-text nut-progress-insidetext"
-        :style="{
-          lineHeight: height,
-          left: `${percentage}%`,
-          transform: `translate(-${+percentage}%,-50%)`,
-          background: textBackground || strokeColor,
-        }"
-      >
-        <span :style="textStyle">{{ percentage }}{{ isShowPercentage ? '%' : '' }} </span>
-      </div>
-      <div
-        v-if="showText && textInside && slotDefault"
-        class="nut-progress-slot"
-        :style="{
-          position: `absolute`,
-          top: `50%`,
-          left: `${percentage}%`,
-          transform: `translate(-${+percentage}%,-50%)`,
-        }"
-      >
-        <slot />
-      </div>
-    </div>
-    <div v-if="showText && !textInside" class="nut-progress-text" :style="{ lineHeight: height }">
-      <template v-if="status === 'text' || status === 'active'">
-        <span :style="textStyle">{{ percentage }}{{ isShowPercentage ? '%' : '' }} </span>
+      <template v-if="props.status === 'text' || props.status === 'active'">
+        <text :style="pureTextStyles">
+          {{ innerText }}
+        </text>
       </template>
-      <template v-else-if="status === 'icon'">
-        <slot name="iconName">
-          <NutIcon name="checked" width="15px" height="15px" custom-color="#439422" />
-        </slot>
+
+      <template v-else-if="props.status === 'icon'">
+        <slot v-if="slots.iconName" name="iconName" />
+
+        <NutIcon
+          v-else
+          name="checked"
+          width="15px"
+          height="15px"
+          custom-color="#439422"
+        />
       </template>
-    </div>
-  </div>
+    </view>
+  </view>
 </template>
 
 <style lang="scss">
