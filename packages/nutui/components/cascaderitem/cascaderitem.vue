16ec15b33e77734f48072a338213cfdc56cfbea5
lixiao <xiaolee9547@163.com>
2024-08-09 11:27:19 +0800

    fix: cascaderItem组件重复快速点击导致选中错误

--- packages/nutui/components/cascaderitem/cascaderitem.vue
+++ packages/nutui/components/cascaderitem/cascaderitem.vue
@@ -173,8 +173,9 @@ function formatTabTitle(pane: CascaderPane) {
 const methods = {
   // 选中一个节点，静默模式不触发事件
   async handleNode(node: CascaderOption, silent?: boolean) {
-    if (node.level !== tabsCursor.value)
+    if (node.level && (node.level !== Number(tabsCursor.value))) {
       return
+    }
 
     const { disabled, loading } = node
 
