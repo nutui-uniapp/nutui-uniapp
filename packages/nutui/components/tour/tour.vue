d3e0517dc741d7ea76770326a1dc6fefb69b8cbd
xiaohe0601 <xiaohe0601@outlook.com>
2024-08-26 22:28:59 +0800

    refactor(tour): defineOptions instead of two script once more

--- packages/nutui/components/tour/tour.vue
+++ packages/nutui/components/tour/tour.vue
@@ -1,20 +1,40 @@
 <script lang="ts" setup>
-import { computed, defineComponent, getCurrentInstance, onMounted, reactive, ref, watch } from 'vue'
-import { CHANGE_EVENT, CLOSE_EVENT, PREFIX, UPDATE_MODEL_EVENT } from '../_constants'
+import { computed, getCurrentInstance, onMounted, reactive, ref, useSlots, watch } from 'vue'
 import NutIcon from '../icon/icon.vue'
 import NutPopover from '../popover/popover.vue'
+import { CHANGE_EVENT, CLOSE_EVENT, UPDATE_MODEL_EVENT } from '../_constants'
 import { useRect } from '../_hooks'
 import { getMainClass, getRandomId } from '../_utils'
 import { tourEmits, tourProps } from './tour'
 
+const COMPONENT_NAME = 'nut-tour'
+
+defineOptions({
+  name: COMPONENT_NAME,
+  options: {
+    virtualHost: true,
+    addGlobalClass: true,
+    styleIsolation: 'shared',
+  },
+})
+
 const props = defineProps(tourProps)
 
 const emit = defineEmits(tourEmits)
+
+const slots = useSlots()
+
 const instance = getCurrentInstance()!
-const refRandomId = getRandomId()
+
+const elId = getRandomId()
+
 const state = reactive({
   showTour: props.modelValue,
   active: 0,
+})
+
+const classes = computed(() => {
+  return getMainClass(props, COMPONENT_NAME)
 })
 
 const showPopup = ref([false])
@@ -23,34 +43,31 @@ const maskRect: any[] = []
 
 const maskStyles = ref<any[]>([])
 
-const classes = computed(() => {
-  return getMainClass(props, componentName)
-})
-
-function maskStyle(index: number) {
+function updateMaskStyle(index: number) {
   const { offset, maskWidth, maskHeight } = props
 
   if (!maskRect[index])
-    return {}
+    return
+
   const { width, height, left, top } = maskRect[index]
 
-  const center = [left + width / 2, top + height / 2] // 中心点 【横，纵】
+  // 中心点：[横，纵]
+  const center = [left + width / 2, top + height / 2]
   const w = Number(maskWidth || width)
   const h = Number(maskHeight || height)
 
-  const styles = {
+  maskStyles.value[index] = {
     width: `${w + +offset[1] * 2}px`,
     height: `${h + +offset[0] * 2}px`,
     top: `${center[1] - h / 2 - +offset[0]}px`,
     left: `${center[0] - w / 2 - +offset[1]}px`,
   }
-  maskStyles.value[index] = styles
 }
 
 function changeStep(type: string) {
   const current = state.active
-  let next = current
 
+  let next = current
   if (type === 'next')
     next = current + 1
   else
@@ -60,6 +77,7 @@ function changeStep(type: string) {
 
   setTimeout(() => {
     showPopup.value[next] = true
+
     state.active = next
   }, 300)
 
@@ -67,11 +85,12 @@ function changeStep(type: string) {
 }
 
 function getRootPosition() {
-  props.steps.forEach(async (item, i) => {
+  props.steps.forEach(async (item, index) => {
     let rect
     // #ifdef H5
     rect = await useRect(item.target)
     // #endif
+
     // #ifndef H5
     // TODO  uniapp微信小程序无法实现，获取不到组件外节点的信息
     rect = await useRect(item.target, instance.root)
@@ -84,133 +103,132 @@ function getRootPosition() {
       rect.right = -rect.right!
     if (rect.bottom! < 0)
       rect.bottom = -rect.bottom!
-
     // #endif
 
-    maskRect[i] = rect
-    maskStyle(i)
+    maskRect[index] = rect
+
+    updateMaskStyle(index)
   })
 }
 
 function close() {
   state.showTour = false
   showPopup.value[state.active] = false
+
   emit(CLOSE_EVENT, state.active)
   emit(UPDATE_MODEL_EVENT, false)
 }
 
-function handleClickMask() {
-  props.closeOnClickOverlay && close()
+function handleMaskClick() {
+  if (props.closeOnClickOverlay) {
+    close()
+  }
 }
+
+watch(() => props.modelValue, (value) => {
+  if (value) {
+    state.active = 0
+    getRootPosition()
+  }
+
+  state.showTour = value
+  showPopup.value[state.active] = value
+})
 
 onMounted(() => {
   setTimeout(() => {
     getRootPosition()
   }, 500)
 })
-
-watch(
-  () => props.modelValue,
-  (val) => {
-    if (val) {
-      state.active = 0
-      getRootPosition()
-    }
-    state.showTour = val
-    showPopup.value[state.active] = val
-  },
-)
-</script>
-
-<script lang="ts">
-const componentName = `${PREFIX}-tour`
-
-export default defineComponent({
-  name: componentName,
-  options: {
-    virtualHost: true,
-    addGlobalClass: true,
-    styleIsolation: 'shared',
-  },
-})
 </script>
 
 <template>
-  <view :class="classes" :style="customStyle">
-    <view v-if="state.showTour" class="nut-tour-masked" @click="handleClickMask" />
+  <view :class="classes" :style="props.customStyle">
+    <view v-if="state.showTour" class="nut-tour-masked" @click="handleMaskClick" />
 
-    <view v-for="(step, i) in steps" :key="i" style="height: 0;">
+    <view v-for="(item, index) in props.steps" :key="index" style="height: 0;">
       <view
         v-if="state.showTour"
-        :id="`nut-tour-popid${i}${refRandomId}`"
+        :id="`nut-tour-popid${index}${elId}`"
         class="nut-tour-mask"
-        :class="[mask ? (showPopup[i] ? '' : 'nut-tour-mask-hidden') : 'nut-tour-mask-none']"
-        :style="maskStyles[i]"
+        :class="[props.mask ? (showPopup[index] ? '' : 'nut-tour-mask-hidden') : 'nut-tour-mask-none']"
+        :style="maskStyles[index]"
       />
+
       <NutPopover
         v-if="state.showTour"
-        v-model:visible="showPopup[i]"
-        :location="step.location || location"
-        :target-id="`nut-tour-popid${i}${refRandomId}`"
-        :bg-color="bgColor"
-        :theme="theme"
+        v-model:visible="showPopup[index]"
+        :location="item.location || props.location"
+        :target-id="`nut-tour-popid${index}${elId}`"
+        :bg-color="props.bgColor"
+        :theme="props.theme"
         :close-on-click-outside="false"
-        :offset="step.popoverOffset || [0, 12]"
-        :arrow-offset="step.arrowOffset || 0"
+        :offset="item.popoverOffset || [0, 12]"
+        :arrow-offset="item.arrowOffset || 0"
         :duration="200"
       >
         <template #content>
-          <slot>
-            <view v-if="type === 'step'" class="nut-tour-content">
-              <view v-if="showTitleBar" class="nut-tour-content-top">
+          <slot v-if="slots.default" />
+
+          <template v-else>
+            <view v-if="props.type === 'step'" class="nut-tour-content">
+              <view v-if="props.showTitleBar" class="nut-tour-content-top">
                 <view @click="close">
-                  <NutIcon name="close" class="nut-tour-content-top-close" size="10px" />
+                  <NutIcon class="nut-tour-content-top-close" name="close" size="10px" />
                 </view>
               </view>
+
               <view class="nut-tour-content-inner">
-                {{ step.content }}
+                {{ item.content }}
               </view>
+
               <view class="nut-tour-content-bottom">
                 <view class="nut-tour-content-bottom-init">
-                  {{ state.active + 1 }}/{{ steps.length }}
+                  {{ state.active + 1 }}/{{ props.steps.length }}
                 </view>
+
                 <view class="nut-tour-content-bottom-operate">
-                  <slot name="prev-step">
+                  <slot v-if="slots['prev-step']" name="prev-step" />
+
+                  <template v-else>
                     <view
-                      v-if="state.active !== 0 && showPrevStep"
+                      v-if="state.active !== 0 && props.showPrevStep"
                       class="nut-tour-content-bottom-operate-btn"
                       @click="changeStep('prev')"
                     >
-                      {{ prevStepTxt }}
+                      {{ props.prevStepTxt }}
                     </view>
-                  </slot>
+                  </template>
+
                   <view
-                    v-if="steps.length - 1 === state.active"
+                    v-if="props.steps.length - 1 === state.active"
                     class="nut-tour-content-bottom-operate-btn active"
                     @click="close"
                   >
-                    {{ completeTxt }}
+                    {{ props.completeTxt }}
                   </view>
 
-                  <slot name="next-step">
+                  <slot v-if="slots['next-step']" name="next-step" />
+
+                  <template v-else>
                     <view
-                      v-if="steps.length - 1 !== state.active"
+                      v-if="props.steps.length - 1 !== state.active"
                       class="nut-tour-content-bottom-operate-btn active"
                       @click="changeStep('next')"
                     >
-                      {{ nextStepTxt }}
+                      {{ props.nextStepTxt }}
                     </view>
-                  </slot>
+                  </template>
                 </view>
               </view>
             </view>
 
-            <view v-if="type === 'tile'" class="nut-tour-content nut-tour-content-tile">
+            <view v-if="props.type === 'tile'" class="nut-tour-content nut-tour-content-tile">
               <view class="nut-tour-content-inner">
-                {{ step.content }}
+                {{ item.content }}
               </view>
             </view>
-          </slot>
+          </template>
         </template>
       </NutPopover>
     </view>
