dfa7787005655d2e69ed94b8bb4a250bf4944dc4
xiaohe0601 <xiaohe0601@outlook.com>
2024-08-26 14:56:50 +0800

    refactor(switch): defineOptions instead of two script once more

--- packages/nutui/components/switch/switch.vue
+++ packages/nutui/components/switch/switch.vue
@@ -1,16 +1,28 @@
 <script lang="ts" setup>
-import type { CSSProperties } from 'vue'
-import { computed, defineComponent, watch } from 'vue'
-import { CHANGE_EVENT, PREFIX, UPDATE_MODEL_EVENT } from '../_constants'
+import { computed, useSlots, watch } from 'vue'
 import NutIcon from '../icon/icon.vue'
+import { CHANGE_EVENT, UPDATE_MODEL_EVENT } from '../_constants'
 import { getMainClass, getMainStyle } from '../_utils'
 import { useFormContext, useFormDisabled } from '../form'
 import { useFormItemContext } from '../formitem'
 import { switchEmits, switchProps } from './switch'
 
+const COMPONENT_NAME = 'nut-switch'
+
+defineOptions({
+  name: COMPONENT_NAME,
+  options: {
+    virtualHost: true,
+    addGlobalClass: true,
+    styleIsolation: 'shared',
+  },
+})
+
 const props = defineProps(switchProps)
 
 const emit = defineEmits(switchEmits)
+
+const slots = useSlots()
 
 const legacyDisabled = computed(() => props.disabled || props.disable)
 
@@ -19,71 +31,64 @@ const formItemContext = useFormItemContext()
 const disabled = useFormDisabled(formContext, legacyDisabled)
 
 const isActive = computed(() => props.modelValue === props.activeValue)
+
 const classes = computed(() => {
-  return getMainClass(props, componentName, {
+  return getMainClass(props, COMPONENT_NAME, {
     [isActive.value ? 'nut-switch-open' : 'nut-switch-close']: true,
-    [`${componentName}-disabled`]: disabled.value,
-    [`${componentName}-base`]: true,
+    [`${COMPONENT_NAME}-disabled`]: disabled.value,
+    [`${COMPONENT_NAME}-base`]: true,
   })
 })
 
 const styles = computed(() => {
-  const obj: CSSProperties = {
+  return getMainStyle(props, {
     backgroundColor: isActive.value ? props.activeColor : props.inactiveColor,
-  }
-  return getMainStyle(props, obj)
+  })
 })
 
-let updateType = ''
+let updateSource: '' | 'click' = ''
 
-function onClick(event: Event) {
+function onClick(event: any) {
   if (disabled.value || props.loading)
     return
-  const value = isActive.value ? props.inactiveValue : props.activeValue
-  updateType = 'click'
-  emit(UPDATE_MODEL_EVENT, value)
-  emit(CHANGE_EVENT, value, event)
+
+  const finalValue = isActive.value ? props.inactiveValue : props.activeValue
+
+  updateSource = 'click'
+
+  emit(UPDATE_MODEL_EVENT, finalValue)
+  emit(CHANGE_EVENT, finalValue, event)
 
   if (formItemContext !== undefined && formItemContext.triggers.value.change)
     formItemContext.validate('change')
 }
 
-watch(
-  () => props.modelValue,
-  (v) => {
-    if (updateType === 'click')
-      updateType = ''
+watch(() => props.modelValue, (value) => {
+  if (updateSource === 'click') {
+    updateSource = ''
+    return
+  }
 
-    else
-      emit(CHANGE_EVENT, v)
-  },
-)
-</script>
-
-<script lang="ts">
-const componentName = `${PREFIX}-switch`
-export default defineComponent({
-  name: componentName,
-  options: {
-    virtualHost: true,
-    addGlobalClass: true,
-    styleIsolation: 'shared',
-  },
+  emit(CHANGE_EVENT, value)
 })
 </script>
 
 <template>
-  <view :class="classes" :style="styles" @click="(onClick as any)">
+  <view :class="classes" :style="styles" @click="onClick">
     <view class="nut-switch-button">
-      <slot v-if="loading" name="icon">
-        <NutIcon name="loading1" :custom-color="activeColor" />
-      </slot>
-      <template v-if="activeText">
+      <template v-if="props.loading">
+        <slot v-if="slots.icon" name="icon" />
+
+        <NutIcon v-else name="loading1" :custom-color="props.activeColor" />
+      </template>
+
+      <template v-if="props.activeText">
         <view class="nut-switch-label open" :class="{ 'nut-hidden': !isActive }">
-          {{ activeText }}
+          {{ props.activeText }}
         </view>
+
         <view class="nut-switch-label close" :class="{ 'nut-hidden': isActive }">
-          {{ inactiveText }}
+          {{ props.inactiveText }}
         </view>
       </template>
     </view>
