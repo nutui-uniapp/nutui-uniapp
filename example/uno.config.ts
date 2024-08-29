d23806f260d618edbf110dc8f050c1729365a5e0
xiaohe0601 <xiaohe0601@outlook.com>
2024-08-29 17:40:14 +0800

    chore: update example unocss config
    (cherry picked from commit 8f3191a4d71542086a294ed0a706a4b03861e532)

--- example/uno.config.ts
+++ example/uno.config.ts
@@ -1,62 +1,38 @@
-import type { Preset, SourceCodeTransformer } from 'unocss'
-
-import {
-  defineConfig,
-  presetAttributify,
-  presetIcons,
-  presetUno,
-  transformerDirectives,
-  transformerVariantGroup,
-} from 'unocss'
-
+import { defineConfig, presetIcons, transformerDirectives, transformerVariantGroup } from 'unocss'
+import { presetUni } from '@uni-helper/unocss-preset-uni'
 import { isH5, isMp } from '@uni-helper/uni-env'
 
-import { presetApplet, presetRemRpx, transformerApplet, transformerAttributify } from 'unocss-applet'
-
-const presets: Preset[] = []
-
-const transformers: SourceCodeTransformer[] = []
-const darkMode = isH5 ? 'class' : 'media'
-
-if (isMp) {
-  presets.push(presetApplet({ dark: darkMode }))
-  presets.push(presetRemRpx())
-  transformers.push(transformerAttributify({ ignoreAttributes: ['block', 'fixed', 'align'] }))
-  transformers.push(transformerApplet())
-}
-else {
-  presets.push(presetUno({ dark: darkMode }))
-  presets.push(presetAttributify())
-  presets.push(presetRemRpx({ mode: 'rpx2rem' }))
-}
-
 export default defineConfig({
-  // cli: {
-  //   entry: {
-  //     patterns: ['components/**/*.{vue,ts}'],
-  //     outFile: 'dist/styles.css',
-  //   },
-  // },
   presets: [
+    presetUni({
+      uno: {
+        dark: isH5 ? 'class' : 'media',
+      },
+      attributify: {
+        ignoreAttributes: ['block', 'fixed', 'align'],
+      },
+    }),
     presetIcons({
-      scale: 1.2,
       warn: true,
+      scale: 1.2,
       extraProperties: {
         'display': 'inline-block',
         'vertical-align': 'middle',
       },
     }),
-    ...presets,
   ],
-  transformers: [transformerDirectives(), transformerVariantGroup(), ...transformers],
+  transformers: [
+    transformerDirectives(),
+    transformerVariantGroup(),
+  ],
   theme: {
     preflightRoot: isMp ? ['page,::before,::after'] : undefined,
     colors: {
       context: 'rgb(var(--ano-c-context))',
       primary: '#fa2c19',
-      success: 'rgba(39, 197, 48,1)',
-      info: 'rgba(73, 101, 242,1)',
+      success: 'rgb(39, 197, 48)',
       warning: 'rgb(255, 190, 13)',
+      info: 'rgb(73, 101, 242)',
       danger: 'rgb(250, 44, 25)',
 
       aGray1: '#f7f8fa',
@@ -68,15 +44,15 @@ export default defineConfig({
       aGray7: '#646566',
       aGray8: '#323233',
 
-      lightBg: '#F3F4F6',
+      lightBg: '#f3f4f6',
       darkBg: '#373739',
-      placeholder: '#DCDCDC',
+      placeholder: '#dcdcdc',
       grayBg: '#484849',
-      darkBd: '#4C4D4F',
-      softBg: '#E5E6EB',
-      darkTitle: '#ECECEC',
-      darkText: '#7C7C7D',
-      darkSubText: 'rgba(255,255,255,.7)',
+      darkBd: '#4c4d4f',
+      softBg: '#e5e6eb',
+      darkTitle: '#ececec',
+      darkText: '#7c7c7d',
+      darkSubText: 'rgba(255, 255, 255, 0.7)',
     },
   },
   rules: [
@@ -91,14 +67,14 @@ export default defineConfig({
     ['pb-safe', { 'padding-bottom': 'env(safe-aren-inset-bottom)' }],
   ],
   shortcuts: {
-    'n-bg': 'bg-[#eff2f5] dark:bg-[#000000]',
-    'n-bg-2': 'bg-white dark:bg-[#1A1A1A]',
-    'n-border': 'border-[#EBEDF0] dark:border-[#3A3A3C]',
-    'n-active': 'bg-[#F2F3F5] dark:bg-[#3A3A3C]',
-    'n-active-h5': 'active:bg-[#F2F3F5] active:dark:bg-[#3A3A3C]',
-    'n-text-color': 'text-[#323233] dark:text-[#F5F5F5]',
-    'n-text-color-2': 'text-[#969799] dark:text-[#707070]',
-    'n-text-color-3': 'text-[#C8C9CC] dark:text-[#4D4D4D]',
+    'n-bg': 'bg-#eff2f5 dark:bg-#000000',
+    'n-bg-2': 'bg-#ffffff dark:bg-#1a1a1a',
+    'n-border': 'border-#ebedf0 dark:border-#3a3a3c',
+    'n-active': 'bg-#f2f3f5 dark:bg-#3a3a3c',
+    'n-active-h5': 'active:bg-#f2f3f5 active:dark:bg-#3a3a3c',
+    'n-text-color': 'text-#323233 dark:text-#f5f5f5',
+    'n-text-color-2': 'text-#969799 dark:text-#707070',
+    'n-text-color-3': 'text-#c8c9cc dark:text-#4d4d4d',
 
     'n-text-mini': 'text-xs',
     'n-text-small': 'text-sm',
@@ -107,7 +83,7 @@ export default defineConfig({
 
     'n-transition': 'transition-all duration-200',
 
-    'n-solid': 'bg-context text-white dark:bg-op80 dark:text-op80',
+    'n-solid': 'bg-context text-#ffffff dark:bg-op80 dark:text-op80',
     'n-outline': 'bg-transparent border border-solid border-context text-context dark:border-op80 dark:text-op80',
     'n-ghost': 'bg-transparent border border-dashed border-context text-context dark:border-op80 dark:text-op80',
     'n-light': 'bg-context text-context bg-op20 dark:text-op80',
@@ -116,5 +92,4 @@ export default defineConfig({
     'n-hairline': 'relative after:content-[\'_\'] after:pointer-events-none after:absolute after:box-border after:top--50% after:right--50% after:bottom--50% after:left--50% after:n-border after:border-solid after:border-0 after:scale-50',
     'n-hairline-bottom': 'n-hairline after:border-b-1px',
   },
-
 })
