8091b998b39b1c47b81c0a752be32d9e77bfbcbc
xiaohe0601 <xiaohe0601@outlook.com>
2024-09-03 11:32:06 +0800

    chore: update deps

--- scripts/uni/build.ts
+++ scripts/uni/build.ts
@@ -1,5 +1,5 @@
 import fs from 'fs-extra'
-import consloa from 'consola'
+import { consola } from 'consola'
 
 async function transformLodash(sourceDir: string, destDir: string) {
   await fs.copy('scripts/uni/lodash', `${destDir}/components/_plugins/lodash`)
@@ -21,37 +21,37 @@ async function build() {
   const destDir = 'example/uni_modules/nutui-uni'
 
   try {
-    consloa.info('开始构建')
+    consola.info('开始构建')
 
-    consloa.info(`开始清空 ${destDir}`)
+    consola.info(`开始清空 ${destDir}`)
     await fs.remove(destDir)
     await fs.ensureDir(destDir)
-    consloa.success('清空完成')
+    consola.success('清空完成')
 
-    consloa.info(`开始复制 ${sourceDir} 至 ${destDir}`)
+    consola.info(`开始复制 ${sourceDir} 至 ${destDir}`)
     await fs.copy(sourceDir, destDir, {
       filter(src) {
         return !src.includes('node_modules')
       },
     })
-    consloa.success('复制完成')
+    consola.success('复制完成')
 
-    consloa.info('开始转换lodash')
+    consola.info('开始转换lodash')
     await transformLodash(sourceDir, destDir)
-    consloa.success('转换完成')
+    consola.success('转换完成')
 
-    consloa.info('开始复制changelog及package.json')
+    consola.info('开始复制changelog及package.json')
     await fs.copy('CHANGELOG.md', `${destDir}/changelog.md`)
     await fs.copy('scripts/uni/package.json', `${destDir}/package.json`)
-    consloa.success('复制完成')
+    consola.success('复制完成')
 
     await fs.createFile(`${destDir}/components/nutui-uni/nutui-uni.vue`)
-    consloa.success('nutui-uni占位文件已创建')
+    consola.success('nutui-uni占位文件已创建')
 
-    consloa.success('构建完成')
+    consola.success('构建完成')
   }
   catch (error) {
-    consloa.error('构建失败', error)
+    consola.error('构建失败', error)
   }
 }
 
