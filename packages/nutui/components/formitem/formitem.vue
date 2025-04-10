ede8264e0e6bbbc1fe54f3ad45bdefd196ec9fc8
cyobason <cyobason@qq.com>
2025-04-10 16:53:36 +0800

    新增右侧箭头，如选择地址、性别等用于识别可选择。

--- packages/nutui/components/formitem/formitem.vue
+++ packages/nutui/components/formitem/formitem.vue
@@ -9,6 +9,7 @@ import { FORM_KEY } from '../form/form'
 import type { FormItemProps } from './formitem'
 import { formitemProps } from './formitem'
 import type { FormItemRule } from './types'
+import NutIcon from '../icon/icon.vue'
 
 const props = defineProps(formitemProps)
 const slots = useSlots()
@@ -95,6 +96,7 @@ export default defineComponent({
     <view class="nut-cell__value nut-form-item__body">
       <view class="nut-form-item__body__slots" :style="bodyStyle">
         <slot />
+        <NutIcon v-if="props.isLink" custom-class="nut-cell__link" name="right" />
       </view>
       <view v-if="formErrorTip[prop] && showErrorMessage" class="nut-form-item__body__tips" :style="errorMessageStyle">
         {{ formErrorTip[prop] }}
