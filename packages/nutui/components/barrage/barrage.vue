87068b8b07fc627050e480a2c951ca7acf2e62a7
xiaohe0601 <xiaohe0601@outlook.com>
2026-01-29 09:39:08 +0800

    chore: update

--- packages/nutui/components/barrage/barrage.vue
+++ packages/nutui/components/barrage/barrage.vue
@@ -1,5 +1,5 @@
 <script lang="ts" setup>
-import type { ComponentInternalInstance } from 'vue'
+import type { CSSProperties } from 'vue'
 import { computed, defineComponent, getCurrentInstance, onMounted, reactive, ref, useSlots, watch } from 'vue'
 import { PREFIX } from '../_constants'
 import { useSelectorQuery } from '../_hooks'
@@ -8,18 +8,18 @@ import { barrageProps } from './barrage'
 
 const props = defineProps(barrageProps)
 
-defineExpose({ add })
+const instance = getCurrentInstance()!
 
-const instance = getCurrentInstance() as ComponentInternalInstance
 const { getSelectorNodeInfo } = useSelectorQuery(instance)
-const classTime = new Date().getTime()
 
 const slotDefault = !!useSlots().default
 
-const timeId = ref(new Date().getTime())
-const danmuList = ref<any>(props.danmu)
-const rows = ref<number>(props.rows)
-const top = ref<number>(props.top)
+const classTime = Date.now()
+
+const timeId = ref(Date.now())
+const danmuList = ref<any[]>(props.danmu)
+const rows = ref(props.rows)
+const top = ref(props.top)
 const speeds = props.speeds
 
 const classes = computed(() => {
@@ -28,57 +28,40 @@ const classes = computed(() => {
   })
 })
 
-onMounted(() => {
-  // #ifdef H5
-  if (slotDefault) {
-    const list = document
-      .getElementsByClassName(`nut-barrage__slotBody${classTime}`)[0]
-      .getElementsByClassName('nut-barrage__item')
-    const childrens = list?.[0]?.children || []
-    danmuList.value = childrens
-  }
-  // #endif
-  runStep()
-})
-
-watch(
-  () => props.danmu,
-  (newValue) => {
-    danmuList.value = [...newValue]
-  },
-)
-
 function add(word: string) {
   danmuList.value = [...danmuList.value, word]
+
   runStep()
 }
 
 function getNode(index: number) {
   setTimeout(async () => {
-    let width = 100
     const dmBodyNodeInfo = await getSelectorNodeInfo(`.nut-barrage--dmBody${timeId.value}`)
-    width = dmBodyNodeInfo?.width || 300
     const itemNodeInfo = await getSelectorNodeInfo(`.nut-barrage__item${index}`)
 
+    const width = dmBodyNodeInfo?.width || 300
     const height = itemNodeInfo?.height
     const nodeTop = `${(index % rows.value) * (height! + top.value) + 20}px`
+
     styleInfo(index, nodeTop, width)
   }, 500)
 }
 
 function runStep() {
-  danmuList.value.forEach((item: any, index: number) => {
+  danmuList.value.forEach((_, index) => {
     getNode(index)
   })
 }
-const styleList = reactive<Record<string, string>[]>([])
+
+const styleList: CSSProperties[] = reactive([])
+
 function styleInfo(index: number, nodeTop: string, width: number) {
   const timeIndex = index - rows.value > 0 ? index - rows.value : 0
   const list = styleList
   const time = list[timeIndex] ? Number(list[timeIndex]['--time']) : 0
   // distance.value = '-' + (speeds / 1000) * 200 + '%';
 
-  const obj = {
+  const obj: CSSProperties = {
     'top': nodeTop,
     '--time': `${props.frequency * index + time}`,
     'animationDuration': `${speeds}ms`,
@@ -87,13 +70,38 @@ function styleInfo(index: number, nodeTop: string, width: number) {
     '--move-distance': `-${width}px`,
   }
   if (slotDefault && danmuList.value[index]?.el) {
-    const orginalSty = danmuList.value[index].el.style
-    danmuList.value[index].el.style = Object.assign(orginalSty, obj)
+    danmuList.value[index].el.style = Object.assign(danmuList.value[index].el.style, obj)
   }
   else {
     styleList.push(obj)
   }
 }
+
+watch(
+  () => props.danmu,
+  (value) => {
+    danmuList.value = [...value]
+  },
+)
+
+onMounted(() => {
+  // #ifdef H5
+  if (slotDefault) {
+    const list = document
+      .getElementsByClassName(`nut-barrage__slotBody${classTime}`)[0]
+      .getElementsByClassName('nut-barrage__item')
+
+    // @ts-expect-error whatever
+    danmuList.value = list?.[0]?.children || []
+  }
+  // #endif
+
+  runStep()
+})
+
+defineExpose({
+  add,
+})
 </script>
 
 <script lang="ts">
@@ -110,23 +118,23 @@ export default defineComponent({
 </script>
 
 <template>
-  <view :class="classes" :style="customStyle">
-    <div>
-      <div :class="[`nut-barrage__slotBody${classTime}`]">
+  <view :class="classes" :style="props.customStyle">
+    <view>
+      <view :class="[`nut-barrage__slotBody${classTime}`]">
         <view
           v-for="(item, index) of danmuList"
           :key="`danmu${index}`"
           class="nut-barrage__item move"
           :class="[`nut-barrage__item${index}`]"
-          :style="styleList[index as number]"
+          :style="styleList[index]"
         >
-          {{ item.length > 8 ? `${item.substr(0, 8)}...` : item }}
+          {{ item.length > 8 ? `${item.slice(0, 8)}...` : item }}
         </view>
-      </div>
-    </div>
+      </view>
+    </view>
   </view>
 </template>
 
 <style lang="scss">
-@import './index';
+@import "./index";
 </style>
